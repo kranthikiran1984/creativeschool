3a4fcfbfcb34374dba4b51530fa28f6538a3255d
kranthikiran1984 <kranthikiran1984@gmail.com>
2014-09-01 17:26:27 -0500

    test pho mysql

--- test.php
+++ test.php
@@ -3,6 +3,15 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+  <?
+        $connect=mysql_connect("us-cdbr-azure-east-a.cloudapp.net","b95a2f9e2a388a","99d6c567") or die("Unable to Connect");
+        mysql_select_db("creativeschoolDB") or die("Could not open the db");
+        $showtablequery="SHOW TABLES FROM dbname";
+        $query_result=mysql_query($showtablequery);
+        while($showtablerow = mysql_fetch_array($query_result))
+        {
+        echo $showtablerow[0]." ";
+        } 
+        ?>
  </body>
 </html>
