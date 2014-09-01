e4d5619ab2216caef3a312c348ec82f1e772e013
kranthikiran1984 <kranthikiran1984@gmail.com>
2014-09-01 17:27:48 -0500

    db

--- test.php
+++ test.php
@@ -6,7 +6,7 @@
   <?
         $connect=mysql_connect("us-cdbr-azure-east-a.cloudapp.net","b95a2f9e2a388a","99d6c567") or die("Unable to Connect");
         mysql_select_db("creativeschoolDB") or die("Could not open the db");
-        $showtablequery="SHOW TABLES FROM dbname";
+        $showtablequery="SHOW TABLES FROM creativeschoolDB";
         $query_result=mysql_query($showtablequery);
         while($showtablerow = mysql_fetch_array($query_result))
         {
