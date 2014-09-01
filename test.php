f382a66f239eac3d01c9a1d88abe9a44a1dc4962
kranthikiran1984 <kranthikiran1984@gmail.com>
2014-09-01 17:44:53 -0500

    as

--- test.php
+++ test.php
@@ -3,15 +3,35 @@
   <title>PHP Test</title>
  </head>
  <body>
+ <ul>
   <?
-        $connect=mysql_connect("us-cdbr-azure-east-a.cloudapp.net","b95a2f9e2a388a","99d6c567") or die("Unable to Connect");
-        mysql_select_db("creativeschoolDB") or die("Could not open the db");
-        $showtablequery="SHOW TABLES FROM creativeschoolDB";
-        $query_result=mysql_query($showtablequery);
-        while($showtablerow = mysql_fetch_array($query_result))
-        {
-        echo $showtablerow[0]." ";
-        } 
+       $user="b95a2f9e2a388a";
+	$password="99d6c567";
+	$database="creativeschoolDB";
+	$server = "us-cdbr-azure-east-a.cloudapp.net";
+
+	$con= mysql_connect($server,$user,$password);
+
+	// Check connection
+	if (mysql_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysql_connect_error();
+	}
+	else
+	{
+		mysql_select_db($con, $database) or die("Unable to select database");
+
+		$query="Select Department_Id, Department_Name FROM department";
+
+		$result = mysql_query($con, $query);
+
+		while($row = mysql_fetch_assoc($result)) {
+		 
+		   echo "<li>".$row['department_name']."</li>"; 
+		}
+
+		mysql_close($con);
+	}
         ?>
+		</ul>
  </body>
 </html>
