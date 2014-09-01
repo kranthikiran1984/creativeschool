11339af9386f6a97253a7c465dc31e4ee7d7e4ac
kranthikiran1984 <kranthikiran1984@gmail.com>
2014-09-01 18:08:03 -0500

    asda

--- departments.php
+++ departments.php
@@ -42,7 +42,7 @@ select the department and click on Go button.
 		<?
         $connect=mysql_connect("us-cdbr-azure-east-a.cloudapp.net","b95a2f9e2a388a","99d6c567") or die("Unable to Connect");
         mysql_select_db("creativeschoolDB") or die("Could not open the db");
-        $showtablequery="Select Department_Id, Department_Name FROM department";
+        $showtablequery="Select Department_Id, Department_Name, Department_Page FROM department";
         $query_result=mysql_query($showtablequery);
         while($showtablerow = mysql_fetch_array($query_result))
         {
