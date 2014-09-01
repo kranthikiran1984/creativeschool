f97375ca7a97220ccc6cab53e3e9acfdd80f57b7
kranthikiran1984 <kranthikiran1984@gmail.com>
2014-09-01 17:16:00 -0500

    index changes

--- departments.js
+++ departments.js
@@ -3,6 +3,4 @@ function MoveToDept(){
     var dep = document.getElementById("depList");
     var selectedVal = dep.options[dep.selectedIndex].value;
     window.location = "./"+selectedVal;
-    
-
 }
